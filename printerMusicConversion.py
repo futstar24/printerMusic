869ec17ac81fbd6d926ef9be785a707c25941fe1
SpyceBoyPepeper <emailcody26@gmail.com>
2023-10-30 21:20:17 -0400

    began line conversion

--- printerMusicConversion.py
+++ printerMusicConversion.py
@@ -5,7 +5,7 @@ noteNames = []
 
 def extract_melody_notes(file_path):
     # Load MIDI file
-    midi_data = pretty_midi.PrettyMIDI(file_path)
+    midi_data = pretty_midi.PrettyMIDI("test_songs/"+file_path)
 
     # Get the instrument (assuming it's the first instrument)
     # You may need to modify this based on your MIDI file structure
@@ -23,7 +23,7 @@ def extract_melody_notes(file_path):
     tempoTimes = tempoChanges[0].tolist()
     tempos = tempoChanges[1].tolist()
     currentTempo = tempos[0] #could be 1
-    for i in range(len(instrument.notes)):
+    for i in range(len(instrument.notes)): #If multiple notes played at a time, reduce to just the highest
         j = i+1
         while j < len(instrument.notes):
             note1 = instrument.notes[i]
@@ -42,7 +42,7 @@ def extract_melody_notes(file_path):
 
     for note in instrument.notes:
         if pastNote == None or not (pastNote.end > note.start):
-            note_name = pretty_midi.note_number_to_name(note.pitch) # starts in eflat major
+            note_name = pretty_midi.note_number_to_name(note.pitch) #Offset to account for different keys
             try:
                 i = tempoTimes.index(note.start)
                 currentTempo = tempos[i]
@@ -72,7 +72,7 @@ def extract_melody_notes(file_path):
     return melody_notes
 
 # Specify the path to your MIDI file
-midi_file_path = "Key_Study_in_E_Major_for_Trumpet.mid"
+midi_file_path = "hot-cross-buns.mid"
 
 # Extract the melody notes from the MIDI file
 melody_notes = extract_melody_notes(midi_file_path)
@@ -82,5 +82,29 @@ melody_notes = extract_melody_notes(midi_file_path)
     print(f"Note: {note[0]}, Duration: {note[1]}, Start Time: {note[2]}, End Time: {note[3]}")
     #print(f"{note[0]} {(round(note[1]))}")'''
 
+def convert_to_print(noteList):
+    #Converts list of notes to print format, write to output file
+    #Parameters: List of notes (each note is a list of [str: note name, int: duration])
+    #Returns: None
+
+    freqSet = set([])
+    for note in noteList: #Check that all notes are between C3 and C6
+        name = note[0]
+        if name != "Rest":
+            if (int(name[-1]) <= 3) or (int(name[-1]) >= 6):
+                if not (name == "C3" or name == "C6" or name == "BS3"): #Edges of playable range
+                    print(f'Error: note {name} out of range')
+                    return
+            if note[0][1] == '#':
+                note[0][1] == 'S'
+            freqSet.add(name)
+
+    print(freqSet)
+        
+
+#melody_notes = [["BS3", 3], ["D5", 2], ["E5", 3]]
+
 for note in melody_notes:
     print(f"{note[0]} {note[1]}")
+
+convert_to_print(melody_notes)
