bfd153d4f56f59f505c068dfc741dad6cbdb1276
futstar24 <jelamberg@gmail.com>
2023-11-03 00:24:56 -0400

    logicupdates

--- printerMusicConversion.py
+++ printerMusicConversion.py
@@ -41,7 +41,6 @@ note_lines = {   "C3" : "               U               I               K    *  
 
 noteNames = set([]) #Set of unique frequencies
 
-keyOffsets = {"G Major":0,"E Major":-2,"F Major":2,}
 
 def extract_melody_notes(file_path):
     # Load MIDI file
@@ -50,17 +49,17 @@ def extract_melody_notes(file_path):
     # Get the instrument (assuming it's the first instrument)
     # You may need to modify this based on your MIDI file structure
     instrument = midi_data.instruments[0]
-    keySigs = midi_data.key_signature_changes
-    print(keySigs)
-    for key in keySigs:
-        print(pretty_midi.key_number_to_key_name((key.key_number)))
 
     # Extract notes and their properties
     melody_notes = []
     tempoChanges = midi_data.get_tempo_changes()
     tempoTimes = tempoChanges[0].tolist()
     tempos = tempoChanges[1].tolist()
-    currentTempo = tempos[0] #could be 1
+    currentTempoIndex = 0
+    currentTempo = tempos[currentTempoIndex]
+
+    removeableNotes = []
+
     for i in range(len(instrument.notes)): #If multiple notes played at a time, reduce to just the highest
         j = i+1
         while j < len(instrument.notes):
@@ -68,56 +67,56 @@ def extract_melody_notes(file_path):
             note2 = instrument.notes[j]
             if note1.start >= note2.start:
                 if note1.pitch > note2.pitch:
-                    instrument.notes.remove(note2)
-                    j-=1
+                    removeableNotes.append(note2)
                 else:
-                    instrument.notes.remove(note1)
-                    j-=1
+                    removeableNotes.append(note1)
             j+=1
-
 
     pastNote = None
 
-    for note in instrument.notes:
-        if pastNote == None or not (pastNote.end > note.start):
-            note_name = pretty_midi.note_number_to_name(note.pitch+keyOffsets[pretty_midi.key_number_to_key_name((keySigs[0].key_number))]) #Offset to account for different keys
+    for i in range(len(instrument.notes)):
+        
+        note = instrument.notes[i]
 
-            try:
-                i = tempoTimes.index(note.start)
-                currentTempo = tempos[i]
-            except:
-                pass
+        currentTempo = tempos[currentTempoIndex]
 
-            duration = ((note.end-note.start)*(currentTempo/60))*4
-            #print(note.end-note.start)
+        if note.pitch < 48 or note.pitch > 84 or note in removeableNotes:
+            instrument.notes[i] = None
+            pastNote = None
+            continue
+        
+        if i == 0 or instrument.notes[i-1] == None or i == len(instrument.notes)-1 or (i < len(instrument.notes)-1 and (instrument.notes[i-1].end <= note.start or note.end != instrument.notes[i-1].start and note.pitch > instrument.notes[i-1].pitch)):
+    
+            note_name = pretty_midi.note_number_to_name(note.pitch)
+
+            if currentTempoIndex < len(tempos)-1 and note.start >= tempoTimes[currentTempoIndex]+1:
+                currentTempoIndex+=1
+                print("tempo switch at note", note_name)
+
             noteNames.add(note_name)
+            duration = ((note.end-note.start)*(currentTempo/60))*4
 
-            if pastNote is not None and round((note.start)*(currentTempo/60),2)-round((pastNote.end)*(currentTempo/60),2) > 0:
+            if pastNote is not None and round((note.start)*(currentTempo/60),2)-round((pastNote.end)*(currentTempo/60),2) > 0.2:
+                
                 restDuration = round((note.start-pastNote.end)*(currentTempo/60)*4)
 
                 while restDuration-4 >= 0:
                     restDuration-=4
                     melody_notes.append(["Rest",4,round((pastNote.end)*(currentTempo/60),2),round((note.start)*(currentTempo/60),2)])
-                #melody_notes.append(("Rest", round(pastNote.end*(currentTempo/60),2), restDuration, currentTempo))
+                
                 if restDuration > 0:
                     melody_notes.append(["Rest",restDuration, round((pastNote.end)*(currentTempo/60),2),round((note.start)*(currentTempo/60),2)])
             
-
             melody_notes.append([note_name, round(duration), round((note.start)*(currentTempo/60),2),round((note.end)*(currentTempo/60),2)])
-            pastNote = note    
+            pastNote = note  
 
     return melody_notes
 
 # Specify the path to your MIDI file
-midi_file_path = "hot-cross-buns.mid"
+midi_file_path = "AllOfMe.mid"
 
 # Extract the melody notes from the MIDI file
 melody_notes = extract_melody_notes(midi_file_path)
-
-# Print the extracted melody notes
-'''for note in melody_notes:
-    print(f"Note: {note[0]}, Duration: {note[1]}, Start Time: {note[2]}, End Time: {note[3]}")
-    #print(f"{note[0]} {(round(note[1]))}")'''
 
 def sortFreqs(freqSet):
     tempList = list(freqSet)
@@ -138,8 +137,8 @@ def convert_to_print(noteList):
     for idx, note in enumerate(noteList): #Check that all notes are between C3 and C6
         name = note[0]
         if name != "Rest":
-            if (int(name[-1]) <= 3) or (int(name[-1]) >= 6):
-                if not (name == "C3" or name == "C6" or name == "BS3"): #Edges of playable range
+            if (int(name[-1]) < 3) or (int(name[-1]) >= 6):
+                if not (name == "C4" or name == "C6" or name == "BS3"): #Edges of playable range
                     print(f'Error: note {name} out of range')
                     return
 
@@ -153,8 +152,7 @@ def convert_to_print(noteList):
         output.write("END\n")
         for note in noteList:
             output.write(f'{note[0]} {note[1]}\n')
-        
-
+    
 for note in melody_notes:
     print(f"{note[0]} {note[1]}")
 
